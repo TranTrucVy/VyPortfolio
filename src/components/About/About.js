f35b9848bdc716704b8a6afac49942f8d3986d2f
TranTrucVy <trantrucvy.work@gmail.com>
2024-11-18 17:23:38 +0700

    Update Portfolio -CV

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,59 +1,3 @@
-// import React from "react";
-// import { Container, Row, Col } from "react-bootstrap";
-// import Particle from "../Particle";
-// import Github from "./Github";
-// import Techstack from "./Techstack";
-// import Aboutcard from "./AboutCard";
-// import laptopImg from "../../Assets/about.png";
-// import Toolstack from "./Toolstack";
-
-// function About() {
-//   return (
-//     <Container fluid className="about-section">
-//       <Particle />
-//       <Container>
-//         <Row style={{ justifyContent: "center", padding: "10px" }}>
-//           <Col
-//             md={7}
-//             style={{
-//               justifyContent: "center",
-//               paddingTop: "30px",
-//               paddingBottom: "50px",
-//             }}
-//           >
-//             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-//               Who <strong className="purple">Am I ?</strong>
-//             </h1>
-//             <Aboutcard />
-//           </Col>
-//           <Col
-//             md={5}
-//             style={{ paddingTop: "120px", paddingBottom: "30px" }}
-//             className="about-img"
-//           >
-//             <img src={laptopImg} alt="about" className="img-fluid" />
-//           </Col>
-//         </Row>
-//         <h1 className="project-heading">
-//           Professional <strong className="purple">Skills and Programming Languages</strong>
-//         </h1>
-
-//         <Techstack />
-
-//         <h1 className="project-heading">
-//           <strong className="purple">Tools and Frameworks</strong> I use
-//         </h1>
-//         <Toolstack />
-
-//         <Github />
-//       </Container>
-//     </Container>
-//   );
-// }
-
-// export default About;
-
-
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
@@ -62,7 +6,7 @@ import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
-import newImage from "../../Assets/newImage.png";  // Đảm bảo bạn đã thêm ảnh vào thư mục Assets
+import exp from "../../Assets/exp.png";  // Đảm bảo bạn đã thêm ảnh vào thư mục Assets
 
 function About() {
   return (
@@ -71,28 +15,61 @@ function About() {
       <Container>
         {/* Chèn ảnh trước phần "Who Am I?" */}
         <Row style={{ justifyContent: "center", padding: "10px" }}>
-        <Col
-            md={7}
-            style={{
-              justifyContent: "center",
-              paddingTop: "30px",
-              paddingBottom: "50px",
-            }}
-          >
-            <h1 className="project-heading" >
-              My <strong className="purple">Experiences?</strong>
-            </h1>
-          </Col>
-          <Col
-            md={5}
-            style={{ paddingTop: "120px", paddingBottom: "30px" }}
-            className="about-img"
-          >
-          </Col>
-        </Row>
+  {/* Row đầu tiên */}
+  <Col
+    md={12}
+    style={{
+      paddingTop: "30px",
+      paddingBottom: "50px",
+      fontSize: "1.25em"
+    }}
+  >
+    {/* H1 căn giữa */}
+    <h1 className="project-heading" style={{ textAlign: "center" }}>
+      My <strong className="purple">Experiences?</strong>
+    </h1>
+    <br />
+    {/* P căn đều hai bên */}
+    <p style={{ textAlign: "justify" }}>
+    - Expert in designing Test cases which are clear, organized by applying the testing techniques.
+    <br></br>
+    - Majoring in testing (Manual and Automation) and Network & system testing skills: application, website testing, mobile testing.
+    <br></br>
+    - Performance Testing, Security Testing
+    <br></br>
+    - Experiences about automation tools: Selenium, Appium (Web Driver, Behavior Driven Development in Python and Java).
+    <br></br>
+    - Build up and improve automation framework: Robot Framework with Selenium2Library, Behavior Driven Development, POM, ...
+    <br></br>
+    - Possess hands-on experience in key business domains, including:
+    <br></br>
+    + Banking & Finance: Expertise in implementing and testing CRM systems to enhance customer engagement.
+    <br></br>
+    + E-Commerce: Proficient in Store Management systems, optimizing online retail operations.
+    <br></br>
+    + Healthcare: Skilled in deploying ERP solutions for hospital management and operational efficiency.
+    <br></br>
+    + Entertainment & Media: Experienced in developing and managing Digital Signage systems for content delivery and advertising. 
+    <br></br>
+    - Experiences with projects applied CI (Continuous Integration) using Jenkins, Teamcity, ...
+    <br></br>
+    - Expert about API testing and Performance testing with some tools: JMeter, Postman …
+    <br></br>
+    - Analysis skills, requirement analysis, data processing, design testing skills, programming skills.
+    </p>
+  </Col>
+  {/* 5 phần trống còn lại */}
+</Row>
+
+        {/* Row thứ hai */}
         <Row style={{ justifyContent: "center", padding: "10px" }}>
           <Col md={12} className="text-center">
-            <img src={newImage} alt="New Image" className="img-fluid" style={{ maxWidth: "100%", height: "auto" }} />
+            <img
+              src={exp}
+              alt="New Image"
+              className="img-fluid"
+              style={{ maxWidth: "100%", height: "auto" }}
+            />
           </Col>
         </Row>
 
@@ -118,6 +95,7 @@ function About() {
             <img src={laptopImg} alt="about" className="img-fluid" />
           </Col>
         </Row>
+
         <h1 className="project-heading">
           Professional <strong className="purple">Skills and Programming Languages</strong>
         </h1>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,6 +12,7 @@ import erp from "../../Assets/Projects//erp.png"
 import tingle from "../../Assets/Projects//tingle.png"
 import digital from "../../Assets/Projects//digital.png"
 import fansome from "../../Assets/Projects//fansome.png"
+import product from "../../Assets/Projects//product.png"
 
 
 function Projects() {
@@ -25,6 +26,22 @@ function Projects() {
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
+                {/* Row thứ hai */}
+        <Row style={{ justifyContent: "center", padding: "10px" }}>
+          <Col md={12} className="text-center">
+            <img
+              src={product}
+              alt="New Image"
+              className="img-fluid"
+              style={{ maxWidth: "100%", height: "auto" }}
+            />
+          </Col>
+        </Row>
+
+        <br></br>
+        <h1 className="project-heading">
+        Detail <strong className="purple">Projects</strong>
+        </h1>
 
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={6} className="project-card">
@@ -32,16 +49,19 @@ function Projects() {
   imgPath={erp}
   title="ERP and B2B Healthcare"
   description={
-    `Web application for managing elderly support centers in Korea. The application allows management of human resources, finances and customer files.
-    • Manage tasks & bugs on Jira
-    • Apply agile scrum model
-    • Work directly with PM to analyze requirements as customer changes or adds new ones and design test cases
-    • Evaluating the impact on the project when new requirements or changes come from the customer
-    • Work directly with PM to design testcase for critical features: Salary slip, Employee workload
-    • Support dev team to reproduce bugs
-    • Execute test and make test report
-    • Testing APIs with Postman
-    • Using dev-tool for UI checking`
+    `ERP and B2B web application designed for managing healthcare support centers in Korea, offering a wide range of features for efficient administration of human resources, finances, patient records, and healthcare services.
+
+    • Design test plan, test cases for critical features like: Administrative and operational, Center and Facility Management, Financial Control and Budget, Human Resources and Account Management, Service and Request Coordination…
+    • Manage tasks and bugs on Jira
+    • Apply Agile Scrum model for projects
+    • Collaborate with the PM to analyze requirements as they change or get added, and design corresponding test cases to ensure the system meets customer needs.
+    • Evaluate the impact of new requirements or changes from the customer on the project
+    • Support the dev team to reproduce and resolve bugs
+    • Execute tests and prepare detailed test reports
+    • Test APIs using Postman to ensure proper data exchange between the system's modules
+    • Use dev tools for UI checking and validating design specifications
+    • Teamsize: 1 PM, 1 BA, 1 Design, 4 Dev, 2 Tester
+    `
   }
   webLink="http://erp.higherpower.co.kr/receiver-payment-management/payment-management?month=9&page=1&year=2024"
 />
@@ -51,7 +71,18 @@ function Projects() {
             <ProjectCard
               imgPath={tingle}
               title="Tingle"
-              description="My personal blog page built with Next.js and Tailwind CSS, rendering content from markdown files with dark mode support."
+              description={`Tingle is a service that enables users to connect with others through video chats in Korea. The platform offers users the opportunity to engage in conversations and make money by participating in calls or chats
+                
+                • Develop test cases and scenarios for: video chat service, ensuring stable connections and smooth communication, user registration, login, and profile management features, and system administrator 
+                • Manage tasks and bugs using Trello
+                • Perform testing across iOS, Android, and Web platforms
+                • Collaborate with PM to analyze customer requirements and design corresponding test cases
+                • Evaluate the impact of any new customer requirements or changes on the project scope
+                • Support the development team in reproducing bugs and troubleshooting issues
+                • Execute tests, document findings, and create detailed test reports, API testing in Jmeter
+                • Test integration features, ensuring smooth video and audio communication between users and verify the user interface for compatibility and design consistency across platforms
+                • Teamsize: 1 PM, 1 BA, 1 Design, 4 Dev, 2 Tester
+              `}
               webLink="https://play.google.com/store/apps/details?id=com.simtalk.livemate&hl=en&gl=US"
             />
           </Col>
@@ -60,7 +91,19 @@ function Projects() {
             <ProjectCard
               imgPath={fansome}
               title="Fansome"
-              description="Online code and markdown editor built with React.js, supporting HTML, CSS, and JavaScript with real-time preview and auto-save using Local Storage."
+              description={`FANSOME is a mobile application designed to create a platform for celebrities, creators, and their fans to interact. The app allows fans to access exclusive content, engage in one-on-one chat sessions, and enjoy live video calls with their favorite celebrities and creators. 
+                
+                • Develop test cases for core features: video chat, content access, live streaming, and subscriptions.
+                • Mangage tasks and bugs in Jira.
+                • Collaborate with PM to analyze requirements and design test cases.
+                • Test user registration, login, and profile management for functionality and security.
+                • Verify video and audio communication integration for smooth performance.
+                • Test content visibility to ensure exclusive access for subscribers.
+                • Assist dev team in reproducing bugs and resolving issues.
+                • Test subscription features for billing, access, and content delivery.
+                • Validate UI consistency across iOS and Android platforms.
+                • Execute tests, document findings, and create detailed test reports.
+                `}
               webLink="https://play.google.com/store/apps/details?id=com.fansome"
             />
           </Col>
@@ -68,19 +111,41 @@ function Projects() {
           <Col md={6} className="project-card">
             <ProjectCard
               imgPath={digital}
-              title="Digital"
-              description="Image classifier model using CNN and Transfer Learning to detect diseased and healthy plant leaves. Achieved 98% accuracy with ResNet34."
-              webLink="https://plant49-ai.herokuapp.com/"
+              title="Digital Signage"
+              description={`An digital signage solution where users can download and play content in real-time. Uploaded by suppliers and can be accessed through product purchases or subscriptions. Include real-time playback, secure content management, and buffer storage management.
+                
+                • Develop test cases for core features (video chat, content access, live streaming, subscriptions)
+                • Collaborate with PM to design test cases based on requirements
+                • Test user registration, login, and profile management for security
+                • Validate video/audio communication features between users
+                • Verify content access for subscribers
+                • Reproduce and report bugs for quick fixes
+                • Test subscription services (billing, access, content delivery)
+                • Validate UI across platforms (iOS, Android)
+                • Execute tests and document findings in reports
+                • Mangage tasks and bugs in Jira
+                • Test API in Jmeter
+                `}
+              webLink="https://play.google.com/store/apps/details?id=com.fansome"
             />
           </Col>
 
           <Col md={6} className="project-card">
             <ProjectCard
               imgPath={suicide}
-              title="Another Projects"
-              description="CRM: ecomerce store.
-                            Restaurant
-              "
+              title="Stepsy"
+              description={`A mobile application designed for learning English, focused on enhancing communication skills and vocabulary. The app evaluates pronunciation to assess the user’s level and tailors a learning path to suit individual progress.
+              
+              • Manage tasks and bugs on Trello
+              • Conduct testing across iOS, Android, and CMS platforms
+              • Create user guides for clients on app and CMS usage
+              • Assess the impact of new or modified requirements on the project
+              • Assist the development team in reproducing and resolving bugs
+              • Perform tests and generate detailed test reports
+              • Work with the PM to establish efficient testing procedures for subscription features
+              • Collaborate closely with the PM to analyze evolving customer requirements and design test cases accordingly
+              • Teamsize: 1 PM, 1BA, 1 Design, 3 Dev, 1 Tester 
+              `}
             />
           </Col>
 
